ed407bb4beba641ae39c1759741af3de7c82daae
Amirhossein-ZareH <zare20212021@gmail.com>
2025-07-20 20:52:17 +0330

    sending file to user

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ bot = telebot.TeleBot("7998495021:AAEMcQyjB3dzCK1_2a3xqosDgabeq4nrFd8")
 
 @bot.message_handler()
 def send_welcome(message):
-    try:
-        if os.path.getsize() < 10:
-            
+    if os.path.getsize('test.txt') < 10:
+        with open('test.txt' , 'a') as file:
+            file.write('Hello')       
+    with open('test.txt', 'rb') as file:
+        bot.send_document(message.chat.id , file)
 
 bot.infinity_polling()

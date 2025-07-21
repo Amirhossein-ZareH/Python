04b2f087a4f4d6d8120090445cc0d3c08f9b4520
Amirhossein-ZareH <zare20212021@gmail.com>
2025-07-21 21:54:19 +0330

    photo sender

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ bot = telebot.TeleBot("7998495021:AAEMcQyjB3dzCK1_2a3xqosDgabeq4nrFd8")
 def send_welcome(message):
     if os.path.getsize('test.txt') < 10:
         with open('test.txt' , 'a') as file:
-            file.write('Hello')       
+            file.write('سن الله بزار برم کربلا')    
     with open('test.txt', 'rb') as file:
         bot.send_document(message.chat.id , file)
+    
+@bot.message_handler(lambda message: message.text == "send image")   
+def send_image(message):
+    with open("test.png" , "rb") as photo:
+        bot.send_photo(message.chat.id , photo)
 
 bot.infinity_polling()

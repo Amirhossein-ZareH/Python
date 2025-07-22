54b105040083629633f047fec39a2ece5fc004f0
Amirhossein-ZareH <zare20212021@gmail.com>
2025-07-22 10:54:08 +0330

    file sender

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import os
 
 bot = telebot.TeleBot("7998495021:AAEMcQyjB3dzCK1_2a3xqosDgabeq4nrFd8")
 
-@bot.message_handler()
-def send_welcome(message):
-    if os.path.getsize('test.txt') < 10:
-        with open('test.txt' , 'a') as file:
-            file.write('سن الله بزار برم کربلا')    
-    with open('test.txt', 'rb') as file:
-        bot.send_document(message.chat.id , file)
+# @bot.message_handler()
+# def send_welcome(message):
+#     if os.path.getsize('test.txt') < 10:
+#         with open('test.txt' , 'a') as file:
+#             file.write(f"hello {message.from_user.first_name} {message.from_user.last_name}\n")    
+#     with open('test.txt', 'rb') as file:
+#         bot.send_document(message.chat.id , file)
     
-@bot.message_handler(lambda message: message.text == "send image")   
+@bot.message_handler(func=lambda message: True)
 def send_image(message):
-    with open("test.png" , "rb") as photo:
-        bot.send_photo(message.chat.id , photo)
+    with open("test.png", "rb") as photo:
+        bot.send_photo(message.chat.id, photo)
+
 
 bot.infinity_polling()

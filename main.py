bedb0dffdc6295eaa90bb08a3b2ec35db9e1530f
Amirhossein-ZareH <zare20212021@gmail.com>
2025-07-22 12:56:53 +0330

    glass keyboard

--- main.py
+++ main.py
@@ -11,10 +11,17 @@ bot = telebot.TeleBot("7998495021:AAEMcQyjB3dzCK1_2a3xqosDgabeq4nrFd8")
 #     with open('test.txt', 'rb') as file:
 #         bot.send_document(message.chat.id , file)
     
-@bot.message_handler(func=lambda message: True)
-def send_image(message):
-    with open("test.png", "rb") as photo:
-        bot.send_photo(message.chat.id, photo)
+# @bot.message_handler(func=lambda message: True)
+# def send_image(message):
+#     with open("test.png", "rb") as photo:
+#         bot.send_photo(message.chat.id, photo)
 
+@bot.message_handler(func= lambda message: True)
+def send_link(message):
+    
+    markup = telebot.types.InlineKeyboardMarkup()
+    button1 = telebot.types.InlineKeyboardButton('hi amir' , url="https://t.me/test_channel")
+    markup.add(button1)
 
+    bot.send_message(message.chat.id, message.text, reply_markup=markup)
 bot.infinity_polling()
